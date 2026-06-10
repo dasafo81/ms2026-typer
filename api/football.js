eeada2905daa2fcae81d1d20091f23ee5780a958
dasafo81 <damianporter1@gmail.com>
2026-06-10 12:08:34 +0200

    Update football.js

--- api/football.js
+++ api/football.js
@@ -1,1 +1,29 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
 
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
+  const { endpoint } = req.query
+  if (!endpoint) {
+    return res.status(400).json({ error: 'Missing endpoint' })
+  }
+
+  const url = `https://api.football-data.org/v4${endpoint}`
+
+  try {
+    const response = await fetch(url, {
+      headers: {
+        'X-Auth-Token': process.env.VITE_FOOTBALL_DATA_KEY
+      }
+    })
+
+    const data = await response.json()
+    return res.status(response.status).json(data)
+  } catch (err) {
+    return res.status(500).json({ error: err.message })
+  }
+}
